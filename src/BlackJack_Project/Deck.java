e8255ab4af3ffefbe22b0169777873f4c4ec52ff
Nidhi1301 <nidhinid@sheridancollege.ca>
2020-03-12 14:09:42 -0400

    Renaming game of cards to deck and adding arraylist of cards.

---
+++ src/BlackJack_Project/Deck.java
@@ -0,0 +1,30 @@
+/**
+ * SYST 17796 Project Winter 2020 Base code.
+ * Students can modify and extend to implement their game.
+ * Add your name as a modifier and the date!
+ * Modifier- Shubh Patel 
+ *           Nidhi Nidhi 
+ *           Manan Pathak
+ *           Faraz Naqvi 
+ */
+package BlackJack_Project;
+
+import java.util.ArrayList;
+
+
+/**
+ * A concrete class that represents any grouping of cards for a Game.
+ *
+ * @author dancye
+ * @modified Paul Bonenfant Jan 2020
+ * @modified Nidhi Nidhi 12/03/2020
+ */
+public class Deck {
+
+    //The group of cards, stored in an ArrayList
+    private ArrayList<Card> cards;
+   
+
+    
+
+}//end class
