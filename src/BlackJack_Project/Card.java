b5c16678cdce73a9ec1949ab5d823e9a6c6c04ab
Shubh228 <pate1995@sheridancollege.ca>
2020-03-12 13:56:20 -0400

    Using enum classes in card class to create a card object.

--- src/BlackJack_Project/Card.java
+++ src/BlackJack_Project/Card.java
@@ -2,28 +2,35 @@
  * SYST 17796 Project Winter 2020 Base code.
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
- * Modifier- Shubh Patel Date- 12th February 2020 
- *           Nidhi Nidhi Date- 13th February 2020
- *           Manan Pathak Date- 13th February 2020
- *           Faraz Naqvi Date- 12th February 2020
+ * Modifier- Shubh Patel
+ *           Nidhi Nidhi
+ *           Manan Pathak
+ *           Faraz Naqvi
  */
 package BlackJack_Project;
 
 /**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
+ * A class to be used as the base Card class for the project.
  *
  * @author dancye
+ * @modifier Shubh Patel - 12/03/2020
  */
-public abstract class Card {
-    //default modifier for child classes
+public class Card {
 
     /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     * Using enum classes to build a card object
      */
-    @Override
-    public abstract String toString();
+
+     private Suit suit;
+     private Value value;
+
+     public Card(Suit suit, Value value){
+    
+        this.value = value;
+        this.suit = suit;
+    
+    }
+
+
 
 }
