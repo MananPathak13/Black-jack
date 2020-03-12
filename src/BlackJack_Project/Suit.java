b294828e7d825c91d9b58eb1482542129b66b902
MananPathak13 <pathaman@sheridancollege.ca>
2020-03-12 13:43:37 -0400

    Deleted Player class and added two enum classes named Value and Suit that contains card attributes

---
+++ src/BlackJack_Project/Suit.java
@@ -0,0 +1,19 @@
+/**
+ * SYST 17796 Project Winter 2020 Base code.
+ * Modifier- Shubh Patel 
+ *           Nidhi Nidhi 
+ *           Manan Pathak
+ *           Faraz Naqvi
+ */
+package BlackJack_Project;
+
+/**
+ * This is an enumeration class that contains different suits of the cards 
+ *
+ * @author Manan Pathak
+ * date: 12/03/2020
+ */
+public enum Suit {
+    DIAMOND,HEARTS,SPADE,CLUB;
+    
+}
---
+++ src/BlackJack_Project/Value.java
@@ -0,0 +1,19 @@
+/**
+ * SYST 17796 Project Winter 2020 Base code.
+ * Modifier- Shubh Patel 
+ *           Nidhi Nidhi 
+ *           Manan Pathak
+ *           Faraz Naqvi
+ */
+package BlackJack_Project;
+
+/**
+ * This is an enumeration class that contains different value of the cards 
+ *
+ * @author Manan Pathak
+ * date: 12/03/2020
+ */
+public enum Value {
+    TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING,ACE;
+    
+}
